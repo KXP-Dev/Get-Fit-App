fb8cb2810af71e9afa07ad61548ec650326a7e39
The-Programming-Mango <kimperez95@outlook.com>
2023-04-24 16:06:38 +1000

    Commit 2 Basic Menu

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
-weight = float(input("Enter your weight in kilograms: "))
-height = float(input("Enter your height in meters: "))
+print ("Welcome to the Get Fit! app")
 
-bmi = weight / (height ** 2)
+def create_menu():
+    print("1. Enter 1 to update your measurements")
+    print("2. Enter 2 to view your measurements")
+    print("3. Enter 3 to calculate your BMI")
+    print("4. Enter 4 to add an excersise")
+    print("5. Enter 5 to view previous excersises")
+    print("6. Enter 6 to exit")
+    choice = input("Enter your selection: ")
+    return choice
 
-print("Your BMI is:", bmi)
+user_choice = ""
+
+while user_choice != "6":
+    user_choice = create_menu()
+
+    if (user_choice == "1"):
+        print("Update measurements ")
+    elif (user_choice == "2"):
+        print("View measurements")
+    elif (user_choice == "3"):
+        print("Calculate BMI")
+    elif (user_choice == "4"):
+        print("Add excersise")
+    elif (user_choice == "5"):
+        print("View excersise history")
+    elif (user_choice == "6"):
+        continue
+    else:
+        print("Invalid Input")
+        
+print("Thank you for using the Get Fit! app")
