95627cede03d669d52560ec92027d5da566843e8
The-Programming-Mango <kimperez95@outlook.com>
2023-04-27 16:52:18 +1000

    Commit 6 BMI Catagory

--- function_3.py
+++ function_3.py
@@ -3,3 +3,11 @@ def calculate_bmi():
     height = float(input("Enter your height in meters: "))
     bmi = round(weight / (height ** 2), 2)
     print("Your BMI is:", bmi)
+    if bmi < 18.5:
+        print("You are underweight.")
+    elif bmi < 25:
+        print("You have a healthy weight.")
+    elif bmi < 30:
+        print("You are overweight.")
+    else:
+        print("You are obese.")
