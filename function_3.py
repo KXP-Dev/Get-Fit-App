2fe75642467c174faa72545712080019bdd5da05
The-Programming-Mango <kimperez95@outlook.com>
2023-04-27 19:41:57 +1000

    Commit 7 BMI Loop

--- function_3.py
+++ function_3.py
@@ -1,13 +1,29 @@
+import csv
+from utils import get_float_input
+
 def calculate_bmi():
-    weight = float(input("Enter your weight in kilograms: "))
-    height = float(input("Enter your height in meters: "))
+    while True:
+        height = get_float_input("Enter your height in cm: ")
+        if height <= 50 or height > 300:
+            print("Please enter a value between 50 and 300 cm. Fun fact, Robert Wadlow was the world's tallest man at 272 cm.")
+        else:
+            break
+    height = height / 100
+    while True:
+        weight = get_float_input("Enter your weight in kg: ")
+        if weight <= 2 or weight > 650:
+            print("Please enter a value between 2 and 650 kg. Fun fact, Jon Brower Minnoch was the world's heaviest man at 635 kg.")
+        else:
+            break
     bmi = round(weight / (height ** 2), 2)
-    print("Your BMI is:", bmi)
     if bmi < 18.5:
-        print("You are underweight.")
+        category = "underweight"
     elif bmi < 25:
-        print("You have a healthy weight.")
+        category = "healthy weight"
     elif bmi < 30:
-        print("You are overweight.")
+        category = "overweight"
     else:
-        print("You are obese.")
+        category = "obese"
+    print("Your BMI is:", bmi)
+    print("You are in the", category, "category.")
+    input("Press Enter to go back to menu...")
