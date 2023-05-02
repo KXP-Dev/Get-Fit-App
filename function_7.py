a76fb1610c382bdf689396c6b3f5493e62763a85
The-Programming-Mango <kimperez95@outlook.com>
2023-05-02 17:40:42 +1000

    Commit 17 Search Measurement Function Compatability With Update Function

--- function_7.py
+++ function_7.py
@@ -1,18 +1,19 @@
 import csv
 from prettytable import PrettyTable
+from utils import get_date_input
 
 def search_measurements(measurements):
     print("Search Measurements")
-    search_term = input("Enter the date of measurement: ")
+    search_term = get_date_input("Enter the date of measurement (dd/mm/yyyy): ")
     with open(measurements, "r") as open_measurements:
         reader = csv.reader(open_measurements)
-        matching_rows = [row for row in reader if search_term.lower() in row [0].lower()]
+        matching_rows = [row for row in reader if search_term.strftime("%d/%m/%Y") == row[0]]
         if len(matching_rows) == 0:
-            print("No measurements found with that date.")
+            print(f"No measurements found with date {search_term.strftime('%d/%m/%Y')}.")
         else:
             table = PrettyTable()
-            table.field_names = ["Date", "Weight", "Height"]
+            table.field_names = ["Date", "Height (cm)", "Weight (kg)"]
             for row in matching_rows:
-                table.add_row(row)
+                table.add_row([row[0], row[1], row[2]])
             print(table)
     input("Press Enter to go back to menu...")
